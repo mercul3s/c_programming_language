3c347ee37280412226e5fd71f610744d5e6857c8
Mercedes Coyle <mcoyle@ulive.com>
2014-02-24 08:12:24 -0800

    fixing reverse string function, added comments

--- chapter1/ex1_19.c
+++ chapter1/ex1_19.c
@@ -12,33 +12,38 @@ main() {
     int len;
     char line[MAXLINE];
 
+    // continue reversing and printing if there is input
     while ((len = getLine(line, MAXLINE)) > 0) {
         reverse(line, len - 1);
         printf("%s\n", line);
     }
-    return 0; 
+    return 0; // default return on successful run
 }
 
+// take string, length of array, and use it to loop over string and switch
+// the places of the letters, reversing it. Temp variable is used to hold
+// the values during swapping.
 void reverse(char s[], int len) {
 
     int i;
     char temp;
-
-    for (i = 0; i > len/2; ++i) {
+    for (i = 0; i <= len/2; ++i) {
         temp = s[i];
         s[i] = s[len-i];
         s[len-i] = temp;
     }
-
     s[len+1] = '\0';
 }
 
+// getLine builds an array of the current input while it is not a space, tab,
+// or newline character. The array is not longer than our maximum line input.
 int getLine(char s[], int lim) {
     int c, i; 
 
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+    // split string on spaces, tabs, newlines
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n' &&c != ' ' && c != '\t'; ++i) {
         s[i] = c; 
     }
-
+    // return our index as the length of the array
     return i; 
 }
